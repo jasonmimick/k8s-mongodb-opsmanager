0d1d489b8f1f54dbd90420c7bc0a4ba76a2dd77c
Jason Mimick <jmimick@gmail.com>
2017-11-06 21:22:07 -0500

    working pods with automation agents

--- install-automation-agent.sh
+++ install-automation-agent.sh
@@ -2,15 +2,12 @@
 # Expects $MMS_GROUP_ID and $MMS_AGENT_APIKEY
 
 #TODO: Error if environment varibles not set.
-#curl -OL https://cloud.mongodb.com/download/agent/automation/mongodb-mms-automation-agent-manager-4.5.1.2319-1.x86_64.rhel7.rpm
 
-AGENT_URL=http://129.33.250.96:8080/download/agent/automation/
+AGENT_URL=${MMS_BASE_URL}/download/agent/automation/
 AGENT=mongodb-mms-automation-agent-manager-
 AGENT_VERSION=3.2.14.2187-1.x86_64.rhel7.rpm
 echo "Downloading $AGENT_URL$AGENT$AGENT_VERSION"
 curl -OL "$AGENT_URL$AGENT$AGENT_VERSION"
-
-#rpm -U mongodb-mms-automation-agent-manager-4.5.1.2319-1.x86_64.rhel7.rpm
 
 echo "Installing $AGENT$AGENT_VERSION"
 rpm -U $AGENT$AGENT_VERSION
@@ -41,8 +38,7 @@ echo "Creating /var/run/mongodb-mms-automation"
 echo "Starting automation agent..."
 su -s "/bin/bash" -c "/opt/mongodb-mms-automation/bin/mongodb-mms-automation-agent \
 -f /etc/mongodb-mms/automation-agent.config \
--pidfilepath /var/run/mongodb-mms-automation/mongodb-mms-automation-agent.pid >> \
-/var/log/mongodb-mms-automation/automation-agent-fatal.log 2>&1 &" mongod
-echo "MongoDB Ops Manager automation agent started. `date`"
+-pidfilepath /var/run/mongodb-mms-automation/mongodb-mms-automation-agent.pid" mongod
+echo "MongoDB Ops Manager automation agent stopped at `date`."
 
 
